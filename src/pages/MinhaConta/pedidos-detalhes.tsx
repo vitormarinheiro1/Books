31565db9e53eb0396a3a20bb37dc20abca11e4f1
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-07-19 14:32:06 -0300

    feat: Chamando a API com axios e utilizando o MAP para exibi-las nos pedidos

--- src/pages/MinhaConta/pedidos-detalhes.tsx
+++ src/pages/MinhaConta/pedidos-detalhes.tsx
@@ -1,24 +1,47 @@
+import axios from "axios";
 import { AbBotao } from "ds-alurabooks-vitor";
+import { useEffect, useState } from "react";
+import { IPedido } from "../../interfaces/IPedido";
 
 export function PedidosDetalhes() {
-    return (
-        <div className="flex flex-col pb-20">
-          <h1 className="font-bold text-2xl pb-5">Pedidos</h1>
+  const [pedidos, setPedidos] = useState<IPedido[]>([]);
+
+  const formatador = Intl.NumberFormat('pt-br',{ style: 'currency', currency: 'BRL' })
+
+  useEffect(() => {
+
+    const token = sessionStorage.getItem("token");
+
+    axios
+      .get<IPedido[]>("http://localhost:8000/pedidos", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((resposta) => setPedidos(resposta.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div className="flex flex-col pb-20">
+      <h1 className="font-bold text-2xl pb-5">Pedidos</h1>
+      {pedidos.map((pedido) => (
+        <div key={pedido.id}>
           <div className="flex justify-between">
             <div className="flex flex-col pl-2">
               <ul>
                 <li>
-                  Pedido: <span className="font-bold">89019031</span>
+                  Pedido: <span className="font-bold">{pedido.id}</span>
                 </li>
                 <li>
-                  Data do pedido: <span className="font-bold">26/05/2024</span>
+                  Data do pedido: <span className="font-bold">{new Date(pedido.data).toLocaleDateString()}</span>
                 </li>
                 <li>
-                  Valor total: <span className="font-bold">R$ 102</span>
+                  Valor total: <span className="font-bold">{formatador.format(pedido.total)}</span>
                 </li>
                 <li>
                   Entrega realizada em:{" "}
-                  <span className="font-bold">01/06/2024</span>
+                  <span className="font-bold">{new Date(pedido.data).toLocaleDateString()}</span>
                 </li>
               </ul>
             </div>
@@ -29,28 +52,8 @@ export function PedidosDetalhes() {
           <div className="py-10">
             <div className="bg-[#002F52] w-[1037px] h-px" />
           </div>
-          <div className="flex justify-between">
-            <div className="flex flex-col pl-2">
-              <ul>
-                <li>
-                  Pedido: <span className="font-bold">89019031</span>
-                </li>
-                <li>
-                  Data do pedido: <span className="font-bold">26/05/2024</span>
-                </li>
-                <li>
-                  Valor total: <span className="font-bold">R$ 102</span>
-                </li>
-                <li>
-                  Entrega realizada em:{" "}
-                  <span className="font-bold">01/06/2024</span>
-                </li>
-              </ul>
-            </div>
-            <div className="flex items-end">
-              <AbBotao texto="Detalhes" tipo="primario" />
-            </div>
-          </div>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
